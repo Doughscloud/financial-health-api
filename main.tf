a2e6247986b1456c257963111ec5b9fd9a487bb5
Douglas Smith <dondoughschemez@macbookpro.mynetworksettings.com>
2025-04-08 23:40:38 -0400

    Update .gitignore to exclude .terraform/ and commit changes to .gitignore and main.tf

--- main.tf
+++ main.tf
@@ -48,4 +48,16 @@ resource "aws_instance" "web_server" {
   user_data = <<-EOF
               #!/bin/bash
               yum update -y
-              amazon-linux-extras install docker 
+              amazon-linux-extras install docker -y
+              sudo service docker start
+              sudo docker run -d -p 5001:5001 schemes3/financial-api:latest
+              EOF
+
+  tags = {
+    Name = "Financial-Health-API"
+  }
+}
+
+output "web_server_public_ip" {
+  value = aws_instance.web_server.public_ip
+}
